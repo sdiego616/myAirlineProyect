8e80861461ecd4caa15a12059d6d3d0f3d8ec117
TOSHIBA <TOSHIBA@DESKTOP-2DGABKI>
2021-02-28 01:40:16 -0500

    Modifico los atributos y constructor de clima.java

--- src/modelo/Clima.java
+++ src/modelo/Clima.java
@@ -4,20 +4,19 @@ package modelo;
 import java.time.LocalDate;
 
 public class Clima {
-    private String fenomenoAtmosferico;
     private double temperatura;
-    private double humedad;
+    private double velocidadViento;
     private double precipitacion;
-    private double tormentaElectrica;
+
     private LocalDate fecha;
 
-    public Clima(String fenomenoAtmosferico, double temperatura, double humedad, double precipitacion, double tormentaElectrica, LocalDate fecha) {
-        this.fenomenoAtmosferico = fenomenoAtmosferico;
+    public Clima(double temperatura, double velocidadViento, double precipitacion, LocalDate fecha) {
         this.temperatura = temperatura;
-        this.humedad = humedad;
+        this.velocidadViento = velocidadViento;
         this.precipitacion = precipitacion;
-        this.tormentaElectrica = tormentaElectrica;
         this.fecha = fecha;
     }
+
+    
     
 }
