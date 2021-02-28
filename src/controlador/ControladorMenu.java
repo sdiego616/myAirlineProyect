ba0248f8ee6049a503fb525a7de831500ead6d2f
TOSHIBA <TOSHIBA@DESKTOP-2DGABKI>
2021-02-28 06:19:05 -0500

    Cree un controlador menu

---
+++ src/controlador/ControladorMenu.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.Clima;
+import vista.Menu;
+import vista.ReservacionA;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class ControladorMenu implements ActionListener {
+    
+    Menu vMenu = new Menu();
+    
+    public ControladorMenu (Menu v)
+    {
+        this.vMenu = v;
+        vMenu.btnClima.addActionListener(this);
+        vMenu.btnReservacion.addActionListener(this);
+        vMenu.btnRutas.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource() == vMenu.btnClima)
+        {
+            abrirClima();
+            System.out.println("hola");
+        }
+        if(ae.getSource() == vMenu.btnReservacion)
+        {
+            abrirReservacion();
+        }
+//        if(ae.getSource() == vMenu.btnRutas)
+//        {
+//            abrirRutas();
+//        }
+    }
+    
+    public void abrirClima()
+    {
+        Clima clima = new Clima();
+        clima.setVisible(true);
+        vMenu.dispose();
+    }
+    
+    public void abrirReservacion()
+    {
+        ReservacionA reserv = new ReservacionA();
+        reserv.setVisible(true);
+        vMenu.dispose();
+    }
+    
+//    public void abrirRutas()
+//    {
+//        Clima clima = new Clima();
+//    }
+    
+}
