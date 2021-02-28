54c7a7434488289f2ea1c28f710babef3ed4bb38
alvarozumba2 <57681168+alvarozumba2@users.noreply.github.com>
2021-02-28 09:50:03 -0500

    Add files via upload

---
+++ src/modelo/TecnicoDAO.java
@@ -0,0 +1,23 @@
+
+package modelo;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import java.util.ArrayList;
+
+public class TecnicoDAO {
+    public void insertarTecnico(Tecnico t)
+    {
+        BasicDBObject documentoTecnico = new BasicDBObject();
+        documentoTecnico.put("Nombre","'" + t.getNombre() + "'");
+        documentoTecnico.put("Apelido","'" + t.getApellido() + "'");
+        documentoTecnico.put("Dirección","'" + t.getDireccion() + "'");
+        documentoTecnico.put("Celular","'" + t.getCelular() + "'");
+        documentoTecnico.put("Dirección","'" + t.getDireccion() + "'");
+        documentoTecnico.put("COrreo","'" + t.getCorreo() + "'");
+        documentoTecnico.put("Codigo técnico","'" + t.getCodigoTecnico() + "'");
+        
+    }
+
+    
+}
