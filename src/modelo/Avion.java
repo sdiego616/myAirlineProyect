7a943b1626c479c1f2bd67b8b449ba2a7c1dd935
alvarozumba2 <57681168+alvarozumba2@users.noreply.github.com>
2021-02-28 13:07:46 -0500

    Update Avion.java

--- src/modelo/Avion.java
+++ src/modelo/Avion.java
@@ -10,5 +10,17 @@ package modelo;
  * @author User
  */
 public class Avion {
-    
+    private String codigoAvion;
+    private double pesoMax;
+    private int pasajerosMax;
+    private double velocidad;
+    private boolean disponible;
+
+    public Avion(String codigoAvion, double pesoMax, int pasajerosMax, double velocidad, boolean disponible) {
+        this.codigoAvion = codigoAvion;
+        this.pesoMax = pesoMax;
+        this.pasajerosMax = pasajerosMax;
+        this.velocidad = velocidad;
+        this.disponible = disponible;
+    }
 }
